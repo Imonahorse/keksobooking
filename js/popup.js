a44b4e1222ab96f0751790a4eee897bdd85bc24f
Alexey <Awotjan@gmail.com>
2021-02-18 01:35:03 +0300

    Убрал функции для особенностей и фото

--- js/popup.js
+++ js/popup.js
@@ -4,39 +4,26 @@ import {translateType} from './util.js';
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const map = document.querySelector('.map__canvas');
 
-const createFeaturesList = (arr, clone) => {
-  const featuresList = clone.querySelector('.popup__features');
-  featuresList.innerHTML = '';
+// const createFeaturesList = (arr, clone) => {
+//   const featuresList = clone.querySelector('.popup__features');
+//   featuresList.innerHTML = '';
+//
+//   arr.forEach((item) => {
+//     const element = document.createElement('li');
+//     element.className = 'popup__feature';
+//     element.classList.add('popup__feature--' + item);
+//     featuresList.appendChild(element);
+//   })
+//
+//   return featuresList;
+// }
 
-  arr.forEach((item) => {
-    const element = document.createElement('li');
-    element.className = 'popup__feature';
-    element.classList.add('popup__feature--' + item);
-    featuresList.appendChild(element);
-  })
-
-  return featuresList;
-}
-
-const createPhotosList = (arr, clone) => {
-  const featuresList = clone.querySelector('.popup__photos');
-  featuresList.innerHTML = '';
-
-  arr.forEach((item) => {
-    const element = document.createElement('img');
-    element.className = 'popup__photo';
-    element.src = item
-    element.width = 45;
-    element.hight = 40;
-    element.alt = 'Фотография жилья';
-    featuresList.appendChild(element);
-  })
-
-  return featuresList;
-}
 
 const createSingleCard = ({offer, author}) => {
   const card = cardTemplate.cloneNode(true);
+  const photoList = card.querySelector('.popup__photos');
+  const featureList = card.querySelector('.popup__features');
+
   card.querySelector('.popup__title').textContent = offer.title;
   card.querySelector('.popup__text--address').textContent = `${offer.address.x}, ${offer.address.y}`;
   card.querySelector('.popup__text--price').textContent = `${offer.price}₽/ночь`;
@@ -45,8 +32,21 @@ const createSingleCard = ({offer, author}) => {
   card.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнат для ${offer.guests} гостей`;
   card.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
   card.querySelector('.popup__type').textContent = translateType(offer.type);
-  createFeaturesList(offer.features, card);
-  createPhotosList(offer.photo, card);
+
+  featureList.innerHTML = '';
+  for(let i=0; i < offer.features.length; i++) {
+    const featureItem = document.createElement('li');
+    featureItem.classList.add('popup__feature', 'popup__feature--' + offer.features[i]);
+    featureList.appendChild(featureItem);
+  }
+
+  photoList.innerHTML = '';
+  for(let i=0; i < offer.photo.length; i++) {
+    const photoItem = cardTemplate.querySelector('.popup__photo').cloneNode(true);
+    photoItem.src = offer.photo[i];
+    photoList.appendChild(photoItem);
+  }
+
   return card;
 };
 
