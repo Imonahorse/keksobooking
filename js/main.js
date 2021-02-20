2df417d09cfff4b2fa5667329c32b4dd6bd6c0c4
Alexey <Awotjan@gmail.com>
2021-02-20 17:15:17 +0300

    Добавил изменение цены апартаментов

--- js/main.js
+++ js/main.js
@@ -3,3 +3,39 @@ import {fillMap} from './popup.js';
 fillMap();
 
 
+const form = document.querySelector('.ad-form');
+const type = form.querySelector('#type');
+const price = form.querySelector('#price');
+
+const minPrice = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
+};
+
+const getNewPrice = (type, obj) => {
+  switch (type.value) {
+    case 'bungalow':
+      price.placeholder = obj.bungalow;
+      price.setAttribute('min', obj.bungalow)
+      return;
+    case'flat':
+      price.placeholder = obj.flat;
+      price.setAttribute('min', obj.flat)
+      return;
+    case 'house' :
+      price.placeholder = obj.house;
+      price.setAttribute('min', obj.house)
+      return;
+    case 'palace':
+      price.placeholder = obj.palace;
+      price.setAttribute('min', obj.palace)
+      return;
+  }
+};
+
+
+type.addEventListener('change', () => {
+  getNewPrice(type, minPrice)
+})
