b447b7efe38af7d55eec8a8610fe2df94f8e0e51
Alexey <Awotjan@gmail.com>
2021-03-17 18:26:44 +0300

    добавил Лоадеры

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
-module.exports ={
+module.exports = {
   entry: './source/js/main.js',
   devtool: 'source-map',
   output: {
     filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'build/js'),
+  },
+  module: {
+    rules: [
+      {test: /\.css$/i, use: ['style-loader', 'css-loader']},
+      {test: /\.(png|svg|jpe?g|gif|woff2?|ttf|eot)$/, use: ['file-loader']},
+    ],
   }
 }
