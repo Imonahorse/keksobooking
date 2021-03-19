9a741e804c6800bb0fe1ed6c5e16e787ac25dcf3
Alexey <Awotjan@gmail.com>
2021-03-19 23:36:27 +0300

    final over9000

--- source/js/util.js
+++ source/js/util.js
@@ -1,3 +1,8 @@
 const isEscEvent = (evt) => evt.key === ('Escape' || 'Esc');
 
-export {isEscEvent};
+const declOfNum = (number, txt=['символ', 'символа','символов']) => {
+  const cases = [2, 0, 1, 1, 1, 2]
+  return txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
+
+export {isEscEvent, declOfNum};
